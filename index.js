e3bd1485c0a3010161e41c5f64eb0317935b3ae6
alalaed <a.alaeddine@live.com>
2021-12-21 10:13:40 +0100

    js

--- index.js
+++ index.js
@@ -62,6 +62,65 @@ obj6 = {
 },
 ]
 
-let obj7 = {
-    
+
+const look = function(data){
+    for( let i = 0; i<data.length; i++){
+        
+    }
 }
+
+const page = function(){
+    const inhalt = `
+    <div id="left-page " class="col-8 mt-5">
+                <div class="row topic no-gutters">
+                    <div class="col-8">
+                        <div id="user">
+                            <img src="/assets/rnb.jpeg" alt="" class="userpic">
+                            <span class="d-inline">Ali Alaeddine</span>
+                        </div>
+                        <div id="heading">
+                            An Extended Break From the Rat Race Has Changed My Life
+                        </div>
+                        <div id="description">
+                            <span class="description">
+                                10 lessons learned that I never want to lose
+                                10 lessons learned that I never want to lose
+                                10 lessons learned that I never want to lose
+                            </span>
+                            <div id="info" class="row ">
+                                <div id="date" class="col-9 pl-0 mr-auto">
+                                    <div class="d-inline">Dec3</div>
+                                    <div class="d-inline ">
+                                        <span>.</span>
+                                    </div>
+                                    <div class="d-inline">4 min read</div>
+                                    <div class="d-inline ">
+                                        <span>.</span>
+                                    </div>
+                                    <div class="d-inline">React</div>
+                                    <div class="d-inline ">
+                                        <span>.</span>
+                                    </div>
+                                    <div class="d-inline">Popular on Medium</div>
+                                   </div> 
+                                    <div class="col-2">
+                                        <i><img src="/assets/icons8-bookmark-64.png" class="info-icon mr-2"></i>
+                                        <i><img src="/assets/ellipsis-icon-9.jpg" class="info-icon ml-1"></i>
+                                    </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div  class="col-4 align-middle pl-2">
+                        <img src="/assets/1_eGhp-DL21v3bYIrnTburew.jpeg" class="image-topic">
+                    </div>
+
+                </div>
+
+            </div>
+    `
+
+
+}
+
