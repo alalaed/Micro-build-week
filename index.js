a3a1f52051c218ec78d339ee910d5de147c53ebe
alalaed <a.alaeddine@live.com>
2021-12-21 08:49:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
-const data = {
+const data = [
+
+obj1 = {
     userName : "Ali Alaeddine",
-    userPicture: ,
+    userPicture : 
     heading: "An Extended Break From the Rat Race Has Changed My Life",
     description:"10 lessons learned that I never want to lose",
     date: "Dec3",
     time: "4 min read",
     about: "react",
     state: "popular on Medium"
-
-}
+},
+obj2 = {
+    userName : "Ali Alaeddine",
+    userPicture:  ,
+    heading: "An Extended Break From the Rat Race Has Changed My Life",
+    description:"10 lessons learned that I never want to lose",
+    date: "Dec3",
+    time: "4 min read",
+    about: "react",
+    state: "popular on Medium"
+},
+obj3 = {
+    userName : "Ali Alaeddine",
+    userPicture: 
+    heading: "An Extended Break From the Rat Race Has Changed My Life",
+    description:"10 lessons learned that I never want to lose",
+    date: "Dec3",
+    time: "4 min read",
+    about: "react",
+    state: "popular on Medium"
+},
+obj4 = {
+    userName : "Ali Alaeddine",
+    userPicture: 
+    heading: "An Extended Break From the Rat Race Has Changed My Life",
+    description:"10 lessons learned that I never want to lose",
+    date: "Dec3",
+    time: "4 min read",
+    about: "react",
+    state: "popular on Medium"
+},
+obj5 = {
+    userName : "Ali Alaeddine",
+    userPicture: 
+    heading: "An Extended Break From the Rat Race Has Changed My Life",
+    description:"10 lessons learned that I never want to lose",
+    date: "Dec3",
+    time: "4 min read",
+    about: "react",
+    state: "popular on Medium"
+},
+obj6 = {
+    userName : "Ali Alaeddine",
+    userPicture: 
+    heading: "An Extended Break From the Rat Race Has Changed My Life",
+    description:"10 lessons learned that I never want to lose",
+    date: "Dec3",
+    time: "4 min read",
+    about: "react",
+    state: "popular on Medium"
+},
+]
