af35ca57938a40def6211a5acab105a57f70829f
alalaed <a.alaeddine@live.com>
2021-12-21 08:42:17 +0100

    javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+const data = {
+    userName : "Ali Alaeddine",
+    userPicture: ,
+    heading: "An Extended Break From the Rat Race Has Changed My Life",
+    description:"10 lessons learned that I never want to lose",
+    date: "Dec3",
+    time: "4 min read",
+    about: "react",
+    state: "popular on Medium"
+
+}
